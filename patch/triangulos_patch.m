3592dd7ee90088a1e6730507f252793dd7c4faf7
Byron Quezada <byque2013@gmail.com>
2018-03-22 12:42:59 +0100

    Aumenté subplots para vectors y faces

--- patch/triangulos_patch.m
+++ patch/triangulos_patch.m
@@ -15,12 +15,18 @@ figure
 patch(x,y,'red')
 
 %% Vértices
+figure
 v = [0 0; 1 0; 0 1];
 f = [1 2 3];
-figure
+subplot(2,2,2);
 patch('Faces', f, 'Vertices', v)
 
 v = [0 0; 0 1; -1 0];
 f = [1 2 3];
-figure
+subplot(2,2,1);
 patch('Faces', f, 'Vertices', v)
+
+v = [0 0; 1 0; 0 1; 0 0; 0 1; -1 0];
+f = [1 2 3; 4 5 6];
+subplot(2,2,[3,4]);
+patch('Faces', f, 'Vertices', v, 'FaceColor', 'red')
