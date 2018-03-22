0961196616901468206684b5313da2fca0d1dca5
Byron Quezada <byque2013@gmail.com>
2018-03-22 12:55:24 +0100

    Aumenté optimización de puntos

--- patch/triangulos_patch.m
+++ patch/triangulos_patch.m
@@ -18,15 +18,20 @@ patch(x,y,'red')
 figure
 v = [0 0; 1 0; 0 1];
 f = [1 2 3];
-subplot(2,2,2);
+subplot(3,2,2);
 patch('Faces', f, 'Vertices', v)
 
 v = [0 0; 0 1; -1 0];
 f = [1 2 3];
-subplot(2,2,1);
+subplot(3,2,1);
 patch('Faces', f, 'Vertices', v)
 
 v = [0 0; 1 0; 0 1; 0 0; 0 1; -1 0];
 f = [1 2 3; 4 5 6];
-subplot(2,2,[3,4]);
+subplot(3,2,[3,4]);
 patch('Faces', f, 'Vertices', v, 'FaceColor', 'red')
+
+v = [0 0; 1 0; 0 1; -1 0];
+f = [1 2 3; 1 3 4];
+subplot(3,2,[5,6]);
+patch('Faces', f, 'Vertices', v, 'FaceColor', 'blue')
